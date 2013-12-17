2322b7d6c884ca00400f679911a01714dc996eeb
Chloe Jackson <jacksonc@vmware.com>
2013-12-17 14:02:20 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 spring-thymeleaf-hello
 ======================
+
+```
+gradle build
+java -jar build/libs/site-0.1.0.jar
+```
+http://localhost:8080/
---
+++ src/main/java/webapp/SiteController.java
@@ -0,0 +1,14 @@
+package site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SiteController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
